f279539abe344907c777041eaf0dc63a6770dabb
Bannterly <111296356+Bannterly@users.noreply.github.com>
2024-10-15 15:03:22 +0100

    Add files via upload

---
+++ VeneClickerRemaster/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace VeneClicker
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}\n\nStack Trace:\n{ex.StackTrace}");
+            }
+        }
+    }
+}
